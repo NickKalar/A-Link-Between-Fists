7ed0a2aa7bda5ca1371411098d4b69e77bf07717
kryptonpwn <edwin321@ymail.com>
2020-03-09 19:18:41 -0700

    added sprite animation and basic collision example
    tied in sprite animation using FXGL, small basic collision example as well.

--- src/application/LinkBetweenFists.java
+++ src/application/LinkBetweenFists.java
@@ -1,87 +1,163 @@
 package src.application;
 
-import java.util.HashMap;
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.components.CollidableComponent;
+import com.almasb.fxgl.input.UserAction;
+import com.almasb.fxgl.physics.BoundingShape;
+import com.almasb.fxgl.physics.CollisionHandler;
+import com.almasb.fxgl.physics.HitBox;
 
-import javafx.animation.Animation;
-import javafx.animation.AnimationTimer;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
-import src.model.Link;
-import src.view.ViewManager;
-
 /**
  * This file is the main class for the program and will be used to execute the game
  * @author Nicholas Kalar
  * @version 2/29/2020
  */
 
-public class LinkBetweenFists extends Application {
-    private HashMap<KeyCode, Boolean> keys = new HashMap<>();
-    Image image = new Image("http://www.zeldagalaxy.com/wp-content/img/sprites/nes/loz/link.png");
-    ImageView imageView = new ImageView(image);
-    Link link = new Link(imageView);
-    static Pane root = new Pane();
-    
+public class LinkBetweenFists extends GameApplication {
+    @Override
+    protected void initSettings(GameSettings settings) { 
+        settings.setWidth(1280);
+        settings.setHeight(720);
+        settings.setTitle("Link Between Fists");
+        settings.setVersion("1.0");
+    }
+
+    private Entity player;
+
+
 
     @Override
-    public void start(Stage primaryStage) {
-        try {
-            root.setPrefSize(1280, 720);
-            root.getChildren().addAll(link);
-
-            Scene scene = new Scene(root);
-            scene.setOnKeyPressed(event->keys.put(event.getCode(),true));
-            scene.setOnKeyReleased(event-> {
-                keys.put(event.getCode(), false);
-            });
-            AnimationTimer timer = new AnimationTimer() {
-                @Override
-                public void handle(long now) {
-                    update();
-                }
-            };
-            timer.start();
-            ViewManager manager = new ViewManager();
-            primaryStage = manager.getMainStage();
-            primaryStage.setTitle("A Link Between Fists");
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    protected void initPhysics() {
+        FXGL.getPhysicsWorld().addCollisionHandler(new CollisionHandler(EntityType.PLAYER, EntityType.BOW) {
+            @Override
+            protected void onCollisionBegin(Entity player, Entity bow) {
+                bow.removeFromWorld();
+            }
+        });
     }
 
-    public static void main(String args[]) {
+    //ties in the animation class with the inputs from keyboard. 
+    @Override
+    protected void initInput() {
+        FXGL.getInput().addAction(new UserAction("Right") {
+            @Override
+            protected void onAction() {
+                player.getComponent(AnimationComponent.class).moveRight();
+            }
+        }, KeyCode.D);
+
+        FXGL.getInput().addAction(new UserAction("Up") {
+            @Override
+            protected void onAction() {
+                player.getComponent(AnimationComponent.class).moveUp();
+            }
+        }, KeyCode.W);
+
+        FXGL.getInput().addAction(new UserAction("Left") {
+            @Override
+            protected void onAction() {
+                player.getComponent(AnimationComponent.class).moveLeft();
+            }
+        }, KeyCode.A);
+
+        FXGL.getInput().addAction(new UserAction("Down") {
+            @Override
+            protected void onAction() {
+                player.getComponent(AnimationComponent.class).moveDown();
+            }
+        }, KeyCode.S);
+    }
+
+    @Override
+    protected void initGame() {
+        player = FXGL.entityBuilder()
+                .type(EntityType.PLAYER) 
+                .at(200, 200) //where to spawn (px,px)
+                .bbox(new HitBox(BoundingShape.box(70, 60))) //creating a hitbox around the player in order to use collisions
+                .with(new AnimationComponent()) //tell entity builder we have created animations for player
+                .with(new CollidableComponent(true)) //collisions = true
+                .buildAndAttach();
+
+        FXGL.entityBuilder()
+        .type(EntityType.BOW)
+        .at(500,200)
+        .viewWithBBox("bow.png")
+        .with(new CollidableComponent(true))
+        .buildAndAttach();
+        
+    }
+
+
+
+    public static void main(String[] args) {
         launch(args);
     }
-
-    // input handling from a keyboard.
-    public void update() {
-        if (isPressed(KeyCode.UP)){
-            link.animation.play();
-            link.animation.setOffsetY(64);
-            link.moveY(-2);
-        } else if (isPressed(KeyCode.DOWN)){
-            link.animation.play();
-            link.animation.setOffsetY(0);
-            link.moveY(2);
-        } else if (isPressed(KeyCode.RIGHT)) {
-            link.animation.play();
-            link.animation.setOffsetY(92);
-            link.moveX(2);
-        } else if (isPressed(KeyCode.LEFT)) {
-            link.animation.play();
-            link.animation.setOffsetY(31);
-            link.moveX(-2);
-        }
-    }
-
-    public boolean isPressed(KeyCode key) {
-        return keys.getOrDefault(key, false);
-    }
 }
+    // private HashMap<KeyCode, Boolean> keys = new HashMap<>();
+    // Image image = new Image("http://www.zeldagalaxy.com/wp-content/img/sprites/nes/loz/link.png");
+    // ImageView imageView = new ImageView(image);
+    // Link link = new Link(imageView);
+    // static Pane root = new Pane();
+    
+
+    // @Override
+    // public void start(Stage primaryStage) {
+    //     try {
+    //         root.setPrefSize(1280, 720);
+    //         root.getChildren().addAll(link);
+
+    //         Scene scene = new Scene(root);
+    //         scene.setOnKeyPressed(event->keys.put(event.getCode(),true));
+    //         scene.setOnKeyReleased(event-> {
+    //             keys.put(event.getCode(), false);
+    //         });
+    //         AnimationTimer timer = new AnimationTimer() {
+    //             @Override
+    //             public void handle(long now) {
+    //                 update();
+    //             }
+    //         };
+    //         timer.start();
+    //         ViewManager manager = new ViewManager();
+    //         primaryStage = manager.getMainStage();
+    //         primaryStage.setTitle("A Link Between Fists");
+    //         primaryStage.setScene(scene);
+    //         primaryStage.show();
+    //     } catch (Exception e) {
+    //         e.printStackTrace();
+    //     }
+    // }
+
+    // public static void main(String args[]) {
+    //     launch(args);
+    // }
+
+    // // input handling from a keyboard.
+    // public void update() {
+    //     if (isPressed(KeyCode.UP)){
+    //         link.animation.play();
+    //         link.animation.setOffsetY(64);
+    //         link.moveY(-2);
+    //     } else if (isPressed(KeyCode.DOWN)){
+    //         link.animation.play();
+    //         link.animation.setOffsetY(0);
+    //         link.moveY(2);
+    //     } else if (isPressed(KeyCode.RIGHT)) {
+    //         link.animation.play();
+    //         link.animation.setOffsetY(92);
+    //         link.moveX(2);
+    //     } else if (isPressed(KeyCode.LEFT)) {
+    //         link.animation.play();
+    //         link.animation.setOffsetY(31);
+    //         link.moveX(-2);
+    //     }
+    // }
+
+    // public boolean isPressed(KeyCode key) {
+    //     return keys.getOrDefault(key, false);
+//     }
+// }
