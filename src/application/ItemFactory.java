d29bda71b45aa7a27886a208fda265f5def4cf8a
kryptonpwn <edwin321@ymail.com>
2020-03-09 22:32:39 -0700

    Added item factory and random spawn
    added item factory to spawn and item when needed. just a bow for now.

---
+++ src/application/ItemFactory.java
@@ -0,0 +1,31 @@
+package src.application;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.EntityFactory;
+import com.almasb.fxgl.entity.SpawnData;
+import com.almasb.fxgl.entity.Spawns;
+
+import javafx.scene.input.MouseButton;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+
+import java.util.Map;
+
+import static com.almasb.fxgl.dsl.FXGL.*;
+
+public class ItemFactory implements EntityFactory {
+    
+    @Spawns("bow")
+    public Entity newBow(SpawnData data) {
+        return entityBuilder()
+            .type(EntityType.BOW)
+            .from(data)
+            .viewWithBBox("bow.png")
+            .collidable()
+            .build();
+    }
+
+
+}
--- src/application/LinkBetweenFists.java
+++ src/application/LinkBetweenFists.java
@@ -2,15 +2,16 @@ package src.application;
 
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.core.math.FXGLMath;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.components.CollidableComponent;
 import com.almasb.fxgl.input.UserAction;
 import com.almasb.fxgl.physics.BoundingShape;
-import com.almasb.fxgl.physics.CollisionHandler;
 import com.almasb.fxgl.physics.HitBox;
-
 import javafx.scene.input.KeyCode;
+import static com.almasb.fxgl.dsl.FXGL.*;
+import javafx.util.Duration;
 /**
  * This file is the main class for the program and will be used to execute the game
  * @author Nicholas Kalar
@@ -28,19 +29,41 @@ public class LinkBetweenFists extends GameApplication {
 
     private Entity player;
 
+    @Override
+    protected void initGame() {
+        player = FXGL.entityBuilder()
+                .type(EntityType.PLAYER) 
+                .at(200, 200) //where to spawn (px,px)
+                .bbox(new HitBox(BoundingShape.box(70, 60))) //creating a hitbox around the player in order to use collisions
+                .with(new AnimationComponent()) //tell entity builder we have created animations for player
+                .with(new CollidableComponent(true)) //collisions = true
+                .buildAndAttach(); //build and attach to game world
+
+        getGameWorld().addEntityFactory(new ItemFactory());
+        run(() -> spawn("bow", FXGLMath.random(10, 1100), FXGLMath.random(10, 700)), Duration.seconds(1));
+
+
+        // FXGL.entityBuilder()
+        // .type(EntityType.BOW)
+        // .at(500,200)
+        // .viewWithBBox("bow.png")
+        // .with(new CollidableComponent(true))
+        // .buildAndAttach();
+        
+    }
+
 
 
     @Override
     protected void initPhysics() {
-        FXGL.getPhysicsWorld().addCollisionHandler(new CollisionHandler(EntityType.PLAYER, EntityType.BOW) {
-            @Override
-            protected void onCollisionBegin(Entity player, Entity bow) {
-                bow.removeFromWorld();
-            }
+        onCollisionBegin(EntityType.PLAYER, EntityType.BOW, (player, bow) -> {
+            bow.removeFromWorld();
         });
-    }
+    }   
 
-    //ties in the animation class with the inputs from keyboard. 
+
+
+    // ties in the animation class with the inputs from keyboard.
     @Override
     protected void initInput() {
         FXGL.getInput().addAction(new UserAction("Right") {
@@ -72,24 +95,16 @@ public class LinkBetweenFists extends GameApplication {
         }, KeyCode.S);
     }
 
-    @Override
-    protected void initGame() {
-        player = FXGL.entityBuilder()
-                .type(EntityType.PLAYER) 
-                .at(200, 200) //where to spawn (px,px)
-                .bbox(new HitBox(BoundingShape.box(70, 60))) //creating a hitbox around the player in order to use collisions
-                .with(new AnimationComponent()) //tell entity builder we have created animations for player
-                .with(new CollidableComponent(true)) //collisions = true
-                .buildAndAttach();
 
-        FXGL.entityBuilder()
-        .type(EntityType.BOW)
-        .at(500,200)
-        .viewWithBBox("bow.png")
-        .with(new CollidableComponent(true))
-        .buildAndAttach();
-        
-    }
+ 
+    // private void spawnBow() {
+    //     FXGL.entityBuilder()
+    //         .type(EntityType.BOW)
+    //         .at(FXGLMath.random(150, getAppWidth() - 100), random(0, getAppHeight()))
+    //         .viewWithBBox("bow.png")
+    //         .collidable()
+    //         .buildAndAttach();
+    // }
 
 
 
